aba608376106cb01d9e87b0f894b5ad515cd45ee
Hamza <hamzaazman6776@gmail.com>
2021-02-08 19:55:11 +0300

    Exercise1 done

---
+++ 2-day-exercises/Exercise-1/exercise1.js
@@ -0,0 +1,31 @@
+let string = '30 Days Of Javascript'
+console.log(string)
+console.log(string.length)
+console.log(string.toUpperCase())
+console.log(string.toLowerCase())
+console.log(string.substr(2,5))
+console.log(string.substr(2,string.length - 1))
+console.log(string.includes('Script'))
+console.log(string.split())
+console.log(string.split(' '))
+let busines = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(busines.split(','))
+console.log(string.replace('Javascript', 'Python'))
+console.log(string.charAt(15))
+console.log(string.charCodeAt(8))
+console.log(string.indexOf('a'))
+console.log(string.lastIndexOf('a'))
+
+let word = 'You cannot end a sentence with because because because is a conjunction'
+console.log(word.indexOf('because'))
+console.log(word.lastIndexOf('because'))
+console.log(string.trim())
+console.log(string.startsWith('30'))
+console.log(string.endsWith('Javascript'))
+console.log(string.match('a'))
+let a = '30 Days Of '
+let b = 'Javascript'
+console.log(a.concat(b))
+console.log(string.repeat(2))
+
+
